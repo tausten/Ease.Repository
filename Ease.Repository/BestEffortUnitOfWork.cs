f263c0dc1b0488c94a6f33c5c4800265d6bbfa9b
Tyler Austen <TylerAusten@gmail.com>
2019-08-30 15:20:19 -0700

    #1 Samples - checkpoint - initial skeleton. Need to fill out with actual sample code next.

--- Ease.Repository/BestEffortUnitOfWork.cs
+++ Ease.Repository/BestEffortUnitOfWork.cs
@@ -10,72 +10,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using ChangeTracking;
 using System.Collections.Concurrent;
+using Ease.Util.Collections.Generic;
 
 namespace Ease.Repository
 {
-    /// <summary>
-    /// TODO: Move this to Ease.Util  (keep `internal` until then)
-    /// 
-    /// A mapping of keys to a collection of values. Takes care of ensuring that an empty collection is present,
-    /// avoiding need for null checks and collection initialization per key prior to use. Eg:
-    ///
-    /// <code>
-    /// var families = new MapOfCollections&lt;string, HashSet&lt;string&gt;, string&gt;();
-    /// families["Doe"].Add("Jane");
-    /// families["Doe"].Add("John");
-    /// var theDoes = string.Join(", ", families["Doe"]);
-    /// </code>
-    ///
-    /// See <seealso cref="MapOfHashSets"/> and <seealso cref="MapOfLists"/> for convenient simplification for
-    /// common some cases.
-    /// </summary>
-    /// <typeparam name="TKey">The lookup key type.</typeparam>
-    /// <typeparam name="TCollection">The collection type to store the values in per key.</typeparam>
-    /// <typeparam name="TValue">The value type.</typeparam>
-    internal class MapOfCollections<TKey, TCollection, TValue>
-        where TCollection : ICollection<TValue>, new()
-    {
-        private readonly Dictionary<TKey, TCollection> _inner = new Dictionary<TKey, TCollection>();
-
-        private TCollection EnsureCollection(TKey key)
-        {
-            if (!_inner.TryGetValue(key, out var coll))
-            {
-                coll = new TCollection();
-                _inner[key] = coll;
-            }
-            return coll;
-        }
-
-        public void Add(TKey key, TValue value)
-        {
-            EnsureCollection(key).Add(value);
-        }
-
-        public void Remove(TKey key, TValue value)
-        {
-            if (_inner.TryGetValue(key, out var coll))
-            {
-                coll.Remove(value);
-            }
-        }
-
-        public TCollection this[TKey key]
-        {
-            get => EnsureCollection(key);
-            set => _inner[key] = value;
-        }
-
-        public void Clear()
-        {
-            _inner.Clear();
-        }
-    }
-
-    internal class MapOfHashSets<TKey, TValue> : MapOfCollections<TKey, HashSet<TValue>, TValue> { }
-
-    internal class MapOfLists<TKey, TValue> : MapOfCollections<TKey, List<TValue>, TValue> { }
-
     /// <summary>
     /// Interface for unit of work pattern where best-effort transaction is to be managed by hand rather than by
     /// underlying store.
---
+++ Samples/SampleDataLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SampleDataLayer
+{
+    public class Class1
+    {
+    }
+}
