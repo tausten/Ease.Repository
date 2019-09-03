e55c2bac230fb715d7ce3f5f952b2819f8d92230
Tyler Austen <TylerAusten@gmail.com>
2019-09-03 11:43:01 -0700

    #1 Got Product-related controller and views in place..  basic SampleCoreWebApp now sorted.

--- Samples/SampleCoreWebApp/Models/ProductDto.cs
+++ Samples/SampleCoreWebApp/Models/ProductDto.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace SampleCoreWebApp.Models
 {
     public class ProductDto
     {
+        [HiddenInput]
         public string PartitionKey { get; set; }
         public Guid Id { get; set; }
         public virtual string ProductName { get; set; }
